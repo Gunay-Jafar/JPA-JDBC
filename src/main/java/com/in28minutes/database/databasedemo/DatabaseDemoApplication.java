dbb4cbdda25baeaa255b8ec8e6b6799da7b37726
Gunay-Jafar <cafarligunay06@gmail.com>
2021-04-20 16:57:10 +0400

    Added first

---
+++ src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -0,0 +1,33 @@
+package com.in28minutes.database.databasedemo;
+
+import com.in28minutes.database.databasedemo.entity.Person;
+import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger logger= LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users --> {}",dao.findAll());
+		logger.info("User id 1-->{}",dao.findById(1));
+		logger.info("Delete 100001-->",dao.deleteById(100001));
+	//	logger.info("Insert 2--> {}",dao.insert(new Person(2,"Aysel","Baku",new Date())));
+	}
+}
